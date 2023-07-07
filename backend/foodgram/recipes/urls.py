97e0e62a5347687331f8ada73211cddfe0e0b42b
Антон Богданович <bogianthony@gmail.com>
2023-07-07 14:18:17 +0300

    modified_recipes_url_flake8_v2.0

--- backend/foodgram/recipes/urls.py
+++ backend/foodgram/recipes/urls.py
@@ -10,24 +10,15 @@ router.register(r'recipes', OurRecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('recipes/download_shopping_cart/',
-         OurRecipeViewSet.as_view(
-             {
-                'get': 'download_shopping_list'
-             }),
+         OurRecipeViewSet.as_view({'get': 'download_shopping_list'}),
          name='download_shopping_list'),
     path('', include(router.urls)),
     path('recipes/<int:pk>/favorite/',
-         OurRecipeViewSet.as_view(
-             {
-                'post': 'create_favorite',
-                'delete': 'delete_favorite'
-             }),
+         OurRecipeViewSet.as_view({'post': 'create_favorite',
+                                   'delete': 'delete_favorite'}),
          name='create_delete_favorite'),
     path('recipes/<int:pk>/shopping_cart/',
-         OurRecipeViewSet.as_view(
-             {
-                'post': 'create_shopping_list',
-                'delete': 'delete_shopping_list'
-             }),
+         OurRecipeViewSet.as_view({'post': 'create_shopping_list',
+                                   'delete': 'delete_shopping_list'}),
          name='create_delete_shopping_list'),
 ]
