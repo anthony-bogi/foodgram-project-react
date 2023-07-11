7aa0212f4b38f6ac4431512304639aab0fb464ad
Антон Богданович <bogianthony@gmail.com>
2023-07-11 11:49:29 +0300

    work_with_infra_v2.0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # praktikum_new_diplom
+# Проект Foodgram - продуктовый помощник
+[![Yambd_final workflow](https://github.com/anthony-bogi/foodgram-project-react/actions/workflows/yamdb_workflow.yml/badge.svg)](https://github.com/anthony-bogi/foodgram-project-react/actions/workflows/yamdb_workflow.yml)
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,19 +1,29 @@
 import os
 
+from dotenv import find_dotenv, load_dotenv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g4829^j=f)y=s8aehlpgfcr!zgc!8enx0plpf3wbz0-d0@imq4'
+SECRET_KEY = os.getenv('SECRET_KEY', default='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '84.201.167.51',
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'https://foodgrambyanthony.ddns.net/',
+    'foodgrambyanthony.ddns.net'
+]
 
 
 # Application definition
@@ -71,8 +81,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='name_bd'),
+        'USER': os.getenv('POSTGRES_USER', default='user_1'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password_password'),
+        'HOST': os.getenv('DB_HOST', default='local_host'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
@@ -114,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.User'
 
