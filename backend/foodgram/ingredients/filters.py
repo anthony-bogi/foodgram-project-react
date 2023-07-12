9d3ad19cd4f8470bfd60eb66957b67b0f6b62171
Антон Богданович <bogianthony@gmail.com>
2023-07-12 12:42:14 +0300

    modified_ingredients_filyer_v2.8

--- backend/foodgram/ingredients/filters.py
+++ backend/foodgram/ingredients/filters.py
@@ -1,26 +1,40 @@
-from django.db.models import Q
-from django.db.models.functions import Lower
-from django_filters.rest_framework import CharFilter, FilterSet
+import django_filters
 
 from .models import Ingredient
 
-
-class IngredientFilter(FilterSet):
-    """Наш фильтр для поиска ингредиентов в списке выпадающих имен."""
-    name = CharFilter(method='our_filter_method')
-
-    def our_filter_method(self, queryset, name, value):
-        if not value:
-            return queryset
-        starts_with_query = Q(name__istartswith=value)
-        contains_query = Q(name__icontains=value)
-        return queryset.annotate(
-            starts_with_match=Lower('name').startswith(Lower(value)),
-            contains_match=Lower('name').contains(Lower(value))
-        ).filter(starts_with_query | contains_query).order_by(
-            '-starts_with_match'
-        )
-
+class IngredientFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains'
+    )
+    
     class Meta:
         model = Ingredient
         fields = ['name']
+
+# from django.db.models import Q
+# from django.db.models.functions import Lower
+# from django_filters.rest_framework import CharFilter, FilterSet
+
+# from .models import Ingredient
+
+
+# class IngredientFilter(FilterSet):
+#     """Наш фильтр для поиска ингредиентов в списке выпадающих имен."""
+#     name = CharFilter(method='our_filter_method')
+
+#     def our_filter_method(self, queryset, name, value):
+#         if not value:
+#             return queryset
+#         starts_with_query = Q(name__istartswith=value)
+#         contains_query = Q(name__icontains=value)
+#         return queryset.annotate(
+#             starts_with_match=Lower('name').startswith(Lower(value)),
+#             contains_match=Lower('name').contains(Lower(value))
+#         ).filter(starts_with_query | contains_query).order_by(
+#             '-starts_with_match'
+#         )
+
+#     class Meta:
+#         model = Ingredient
+#         fields = ['name']
