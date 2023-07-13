a2c24810c437eb3c3464b4bada22252c7ab06f40
Антон Богданович <bogianthony@gmail.com>
2023-07-13 13:09:41 +0300

    modified_recipes_permissions_v2.17

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsRecipeAuthorOrReadOnly(permissions.BasePermission):
+    """Только для автора рецепта."""
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
